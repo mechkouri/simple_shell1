d4594814c868ab6f171ff7d95d6384f13c54405a
mechkouri <saidmechkouri@gmail.com>
2023-09-14 04:15:15 -0700

    shell file created

---
+++ shell.h
@@ -0,0 +1,54 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+void    fd_putstr(char *str, int fd);
+char    *_substr(char *str, int s, int e);
+char	*_strdup(char *str);
+int	_strlen(char *str);
+void    free_split(char **arr);
+char	*_strjoin(char *s1, char *s2);
+char	**_split(char *str, char c);
+char	*_strstr(char *str, char *needed);
+int	_strcmp(char *s1, char *s2);
+
+/**
+ * struct s_shell - a struct that conatains all our vairaible
+ * @av: the argument av for the main func
+ * @line: the cmd that user enter
+ * @len: the size that should readline read
+ * @read: the size that readline return after read
+ * @args: two dimen array that conatine the cmds
+ * @pid: the proccess id when we create it with fork()
+ * @exit_s: the exit status
+ * @ret: retrn value
+ * @error: index of error
+ */
+
+struct s_shell
+{
+	char	**av;
+	char    *line;
+	size_t  len;
+	ssize_t read;
+	char    **args;
+	int     pid;
+	int	exit_s;
+	int	ret;
+	int	error;
+};
+
+/* t_shell - Typedef of struct s_shell */
+
+typedef struct s_shell t_shell;
+
+int	build_path(t_shell *ptr, char **env);
+void	_putchar(char c);
+void	_putnbr(int nb);
+int	is_built_in(t_shell *ptr, char **env);
+
+#endif 
