7ae9c4483ab56ce677054314eae3ec4fb0df292a
mechkouri <saidmechkouri@gmail.com>
2023-09-15 02:24:55 -0700

    strcmp created

---
+++ _strcmp.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+	 * _strcmp - compare between two string
+ * Return: 0 if they are equal or !0 if not
+ */
+
+int	_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (-1);
+	}
+	return (0);
+}
